980289e6f9bbbb88b4330d2fe923ad6af7b078b4
John K Best <John.Best@dfw.wa.gov>
2024-02-20 17:18:05 -0800

    Add simulation summary functions

--- phos-sim4.R
+++ phos-sim4.R
@@ -210,8 +210,26 @@ number_handled <- function(res) {
   list(marked = mark, trapped = trap, recaptured = recap)
 }
 
+sim_summarize <- function(res) {
+  tibble(origin = res$origin,
+         sex = res$sex,
+         resid = res$resid,
+         final_state = res$zfull[, ncol(res$zfull)],
+         n_capture = apply(res$y, 1, \(r) sum(r %in% c(2, 3))),
+         captured = n_capture > 0,
+         recaptured = n_capture > 1,
+         trapped = final_state == 4)
   }
 
+sim_handled <- function(res) {
+  sim_summarize(res) |>
+    summarize(
+      n = n(),
+      n_cap = sum(captured),
+      n_recap = sum(recaptured),
+      n_trapped = sum(trapped),
+      .by = c(final_state)
+    )
 }
 
 prep_data <- function(res) {
